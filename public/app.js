3247592d1e689df9d329483b5633b478356bacf7
mc0918 <mcampbell0918@gmail.com>
2019-04-21 16:09:44 -0500

    added modal

--- public/app.js
+++ public/app.js
@@ -4,5 +4,15 @@ $(".article__title").on("click", function() {
 
   $.get({ url: `/articles/${thisId}` }).then(data => {
     console.log(data);
+    $("#comments").append("<input id='title-input' name='title' ");
+    $("#comments").append("<textarea id='body-input' name='body'></textarea>");
   });
+});
+
+$("#exampleModal").on("show.bs.modal", event => {
+  var button = $(event.relatedTarget);
+  var recipient = button.data("test");
+  var modal = $(this);
+  modal.find(".modal-title").text("New message to " + recipient);
+  modal.find(".modal-body input").val(recipient);
 });
