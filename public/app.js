ac5dc4250e64d8f944a36e9dfd278576568ec13b
mc0918 <mcampbell0918@gmail.com>
2019-04-21 15:09:41 -0500

    articles appending to handlebars files

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
-console.log("hi there :D");
+$(".article__title").on("click", function() {
+  //$("#comments").empty() //un-comment this when comment part is made
+  var thisId = $(this).attr("data-id");
+
+  $.get({ url: `/articles/${thisId}` }).then(data => {
+    console.log(data);
+  });
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var db = require("../models");
 
 router.get("/", function(req, res) {
-  res.render("index");
+  db.Article.find({}, function(err, data) {
+    console.log(data);
+    res.render("index", { articles: data });
+  });
 });
 
 module.exports = router;
